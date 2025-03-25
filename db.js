94084a07814d64c4adecce92b1c16fc5d417171c
Felipe Possari <fcpfelipe@hotmail.com>
2025-03-25 11:41:11 -0300

    Update db to use file system

--- db.js
+++ db.js
@@ -1,31 +1,43 @@
-const {v4} = require('uuid');
+const { v4 } = require('uuid');
+const fs = require("fs");
+const FILE_PATH = require("path").join(__dirname, "db.json");
 
 global.users = [];
 
-function findUsers(){
-    return global.users;
+function findUsers() {
+    try {
+        return require("./db.json");
+    } catch (e) {
+        return [];
+    }
 }
 
-function addUser(user){
+function addUser(user) {
+    const users = findUsers();
     user.id = v4();
-    global.users.push(user);
+    users.push(user);
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users));
     return user;
 }
 
-function findUserById(id){
-    return global.users.find(user => user.id === id);
+function findUserById(id) {
+    return findUsers().find(user => user.id === id);
 }
 
-function updateUser(id, user){
+function updateUser(id, user) {
+    const users = findUsers();
     user.id = id;
-    const index = global.users.findIndex(user => user.id === id);
-    global.users[index] = user;
+    const index = users.findIndex(user => user.id === id);
+    users[index] = user;
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users));
     return user;
 }
 
-function deleteUser(id){
-    const index = global.users.findIndex(user => user.id === id);
-    global.users.splice(index, 1);
+function deleteUser(id) {
+    const users = findUsers();
+    const index = users.findIndex(user => user.id === id);
+    users.splice(index, 1);
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users));
 }
 
 module.exports = {
