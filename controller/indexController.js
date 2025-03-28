c8643e572c9d9611ac1909f4c884b616da71bc04
Felipe Possari <fcpfelipe@hotmail.com>
2025-03-28 08:37:44 -0300

    Update index controller

---
+++ controller/indexController.js
@@ -0,0 +1,7 @@
+function index(req, res, next) {
+    res.sendStatus(200);
+}
+
+module.exports = {
+    index
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const indexController = require('../controller/indexController');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.sendStatus(200);
-});
+router.get('/', indexController.index);
 
 module.exports = router;
